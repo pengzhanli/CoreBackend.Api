2d8819e47311a0e3e02dc5c0b97b37c97a9668d2
pengzhanli <1824264301@qq.com>
2019-09-18 10:28:34 +0800

    Add ProductService

---
+++ CoreBackend.Api/Controllers/MaterialController.cs
@@ -0,0 +1,42 @@
+﻿using CoreBackend.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBackend.Api.Controllers
+{
+    //// 和主Model的Controller前缀一样
+    [Route("api/product")]
+    public class MaterialController : Controller
+    {
+        [HttpGet("{productId}/materials")]
+        public IActionResult GetMaterials(int productId)
+        {
+            var product = ProductService.Current.Products.SingleOrDefault(x => x.Id == productId);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product.Materials);
+        }
+
+        [HttpGet("{productId}/materials/{id}")]
+        public IActionResult GetMaterial(int productId, int id)
+        {
+            var product = ProductService.Current.Products.SingleOrDefault(x => x.Id == productId);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            var material = product.Materials.SingleOrDefault(x => x.Id == id);
+            if (material == null)
+            {
+                return NotFound();
+            }
+            return Ok(material);
+        }
+
+    }
+}
---
+++ CoreBackend.Api/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using CoreBackend.Api.Model;
+using CoreBackend.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBackend.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(ProductService.Current.Products);
+        }
+
+        [Route("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            var result = ProductService.Current.Products.SingleOrDefault(x => x.Id == id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+
+    }
+}
